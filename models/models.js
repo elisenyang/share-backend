a5b5f63bde0c45875ca5c87afa0792d72ce44e61
Elise Yang <elisenyang@Elises-MacBook-Pro-2.local>
2017-09-19 16:45:12 -0400

    before redux

--- models/models.js
+++ models/models.js
@@ -1,13 +1,16 @@
 var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
-  username: {
+  email: {
     type: String,
     required: true
   },
   password: {
     type: String,
     required: true
+  },
+  userInfo: {
+    type: Object
   }
 });
 
--- server.js
+++ server.js
@@ -29,11 +29,64 @@ app.get('/posts', function(req,res) {
     if (err) {
       res.status(500).send({error: 'Posts could not be found'})
     }
-    console.log(docs)
     res.status(200).json(docs)
   })
 })
 
+app.post('/userInfo', function(req, res) {
+  var posts = req.body.posts
+  function retrieveUserInfo(id, callback) {
+    User.findById(id, function(err, user) {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, user);
+      }
+    });
+  }
+  Promise.all(posts.map((post)=> {
+    return retrieveUserInfo(post.user.id, function(err, user) {
+      if (err) {
+        console.log(err)
+      }
+      post.user.userInfo = user.userInfo
+    })
+  })).then((updatedPosts)=> {
+    console.log(updatedPosts)
+    res.json({success: posts})
+  })
+})
+
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+app.post('/register', function(req,res) {
+
+  let newUser = new User({
+    email: req.body.email,
+    password: req.body.password,
+    userInfo: {
+      year: req.body.userInfo.year,
+      school: req.body.userInfo.school,
+      gender: req.body.userInfo.gender,
+      age: req.body.userInfo.age
+    }
+  })
+  
+  if (req.body.email && req.body.password) {
+    newUser.save(function(err) {
+      if (err) {
+        res.status(500).json({error: 'Registration Failed. Please try again'})
+      } else {
+        res.status(200).json({success: 'Registration successful'})
+      }
+    })
+  } else if (!req.body.email) {
+    res.json({error: 'You must enter a valid email'})
+  } else if (!req.body.password) {
+    res.json({error: 'You must provide a valid password'})
+  }
+})
 
 
 var port = process.env.PORT || 3000;
