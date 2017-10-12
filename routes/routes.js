a0b83b2486ac43e5c9e0ec7cc7ba3c764cabe9d0
Elise Yang <elisenyang@levy-ve500-0418.apn.wlan.upenn.edu>
2017-10-12 17:03:56 -0400

    jdiejodq

--- routes/routes.js
+++ routes/routes.js
@@ -20,7 +20,7 @@ router.use(function(req, res, next){
 router.get('/posts', function(req,res) {
   console.log('id', req.user._id)
   if (req.query.myposts) {
-    Post.find({user: {$eq: {id: req.user_id}}}, function(err, posts) {
+    Post.find({"user.id": "59c084882bff41249ac6bfab"}, function(err, posts) {
       console.log('HERE', posts)
     })
   } else {
