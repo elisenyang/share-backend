3fa668509e5855bb8345beefec7c659db5d89543
Elise Yang <elisenyang@Elises-MacBook-Pro-2.local>
2017-10-12 21:44:50 -0400

    jfiwojfw

--- routes/routes.js
+++ routes/routes.js
@@ -82,7 +82,8 @@ router.get('/posts', function(req,res) {
              return -1
            }
         })
-        res.json(resp.slice(req.query.page*10-10, req.query.page*10))
+        var sliced = resp.slice(req.query.page*10-10, req.query.page*10)
+        res.json(sliced)
       })
     })
   }
