da921a5e8c06adda14e08483419ae6affe8a58a2
Elise Yang <elisenyang@Elises-MacBook-Pro-2.local>
2018-03-16 15:04:34 -0400

    edit myHearts route

--- routes/routes.js
+++ routes/routes.js
@@ -458,20 +458,14 @@ router.post('/seeWarning', function(req, res) {
 router.get('/myHearts', function(req,res) {
   User.findById(req.user._id, function(err, user) {
     var postsArr = []
-    Post.find(function(err, posts) {
-      user.hearts.forEach(heart => {
-        posts.forEach(post => {
-          if (heart.post === post.id) {
-            postsArr.push(post)
-          }
-        })
+    user.hearts.forEach(heart => {
+      Post.findById(heart.post, function(err, post) {
+        postsArr.push(post)
       })
     })
-    return postsArr
-  }).then(postsArr => {
-    console.log('respppp', [...postsArr])
-    res.json(postsArr)
+    console.log('POSTSARR', postsArr)
   })
+  res.json([])
 })
 
 
